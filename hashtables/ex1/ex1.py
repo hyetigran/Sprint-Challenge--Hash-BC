dff63455c94e6eba513c4dbd659b1eaeeb08fad6
hyetigran <tigran.asriyan@gmail.com>
2020-03-13 13:23:01 -0400

    complete ex1 and ex2

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -12,8 +12,32 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
+    if len(weights) <= 1:
+        return None
 
-    return None
+    result = []
+    table = {}
+
+    for i in range(0, len(weights)):
+        sum_weight = limit - weights[i]
+
+        table[weights[i]] = sum_weight
+
+    for i in range(0, len(weights)):
+        if limit - weights[i] in table:
+            result.insert(0, i)
+
+    return result
+    # index = 0
+
+    # for item in weights:
+    #     weights_sum = limit - item
+    #     existing_key = hash_table_retrieve(ht, item)
+
+    #     if (existing_key == 0 or existing_key) and (limit - item == weights_sum):
+    #         return (index, existing_key)
+    #     hash_table_insert(ht, item, index)
+    #     index += 1
 
 
 def print_answer(answer):
